7e36d88e85d30ce178e8766de5e4d67dfff0e262
Skander Haouam <44363467+madnaks@users.noreply.github.com>
2020-05-09 23:58:47 +0100

    Initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB6KmJg68IWWCcUKnBhXJH1byGUgArC1Ks",
+    authDomain: "ionic-4-firebase-login-6556b.firebaseapp.com",
+    databaseURL: "https://ionic-4-firebase-login-6556b.firebaseio.com",
+    projectId: "ionic-4-firebase-login-6556b",
+    storageBucket: "ionic-4-firebase-login-6556b.appspot.com",
+    messagingSenderId: "515070538843",
+    appId: "1:515070538843:web:0086e31435bc36ad99bb7e"
+  }
+ };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
